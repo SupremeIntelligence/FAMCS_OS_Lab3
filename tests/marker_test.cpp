b50b0b75b575845323893ca054be1aca8fc8d0a5
Supreme Intelligence <supremeintelligence1602@mail.ru>
2024-12-18 00:02:47 +0300

    Downloaded and connected Boost for unit-testing.

---
+++ tests/marker_test.cpp
@@ -0,0 +1,14 @@
+#define BOOST_TEST_MODULE VsidCommonTest
+#include <boost/test/unit_test.hpp>
+#include <windows.h>
+#include"globals.h"
+
+struct InitTestData
+{
+	InitTestData()
+	{
+		
+	}
+};
+
+
