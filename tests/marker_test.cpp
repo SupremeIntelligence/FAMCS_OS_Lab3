ec693179ab4f87b0e9eaa7d60119eca4d765369d
Supreme Intelligence <supremeintelligence1602@mail.ru>
2024-12-19 02:37:25 +0300

    Added 3 unit tests for testing of thread laucnh, array filling and thread stop.

--- tests/marker_test.cpp
+++ tests/marker_test.cpp
@@ -2,6 +2,7 @@
 #include <boost/test/included/unit_test.hpp>
 #include <windows.h>
 #include"globals.h"
+#include"marker.cpp"
 
 std::vector<int> numbers;
 std::vector<HANDLE> stopEvents;
@@ -45,13 +46,67 @@ struct InitTestData
 	}
 };
 
+BOOST_FIXTURE_TEST_SUITE(MarkerTestsSuite, InitTestData)
+
 BOOST_AUTO_TEST_CASE(MarkerStartTest)
 {
+	int threadID = 0;
+	HANDLE thread = CreateThread(nullptr, 0, marker, &threadID, 0, nullptr);
+	BOOST_REQUIRE(thread != nullptr);
 
+	SetEvent(startEvent);
+
+	WaitForSingleObject(doneEvents[threadID], INFINITE);
+
+	EnterCriticalSection(&cs);
+	bool foundID = false;
+	for (int val : numbers) {
+		if (val == threadID) {
+			foundID = true;
+			break;
+		}
+	}
+	LeaveCriticalSection(&cs);
+
+	BOOST_CHECK(foundID);
+	CloseHandle(thread);
 }
 
-BOOST_AUTO_TEST_CASE(MarkerStopTest)
+BOOST_AUTO_TEST_CASE(MarkerArrayFillingTest)
 {
+	int threadID = 1;
+	HANDLE thread = CreateThread(nullptr, 0, marker, &threadID, 0, nullptr);
+	BOOST_REQUIRE(thread != nullptr);
 
+	SetEvent(startEvent);
+
+	WaitForSingleObject(doneEvents[threadID], INFINITE);
+
+	EnterCriticalSection(&cs);
+	int count = 0;
+	for (int val : numbers) {
+		if (val == threadID) {
+			count++;
+		}
+	}
+	LeaveCriticalSection(&cs);
+
+	BOOST_CHECK_GT(count, 0);
+	CloseHandle(thread);
 }
 
+BOOST_AUTO_TEST_CASE(MarkerStopTest) {
+	int threadID = 1;
+	HANDLE thread = CreateThread(nullptr, 0, marker, &threadID, 0, nullptr);
+	BOOST_REQUIRE(thread != nullptr);
+
+	SetEvent(startEvent);
+
+	SetEvent(stopEvents[threadID]);
+	WaitForSingleObject(doneEvents[threadID], INFINITE);
+
+	BOOST_CHECK(WaitForSingleObject(thread, 0) == WAIT_OBJECT_0);
+	CloseHandle(thread);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
